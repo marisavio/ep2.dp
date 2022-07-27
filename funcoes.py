aafb7e52d88950083687aab9cd72ca14157f8b97
marisavio <marianasavio16@gmail.com>
2022-07-27 15:07:57 -0300

    funcao adicionada

--- funcoes.py
+++ funcoes.py
@@ -14,4 +14,41 @@ def transforma_base(lista):
         i += 1
     return nd 
 
+def valida_questao(questao):
+    problemas = {}
+
+    for chave in ['titulo', 'nivel', 'opcoes', 'correta']:
+        if chave not in questao.keys():
+            problemas[chave] = 'nao_encontrado'
+
+    if len(questao.keys()) != 4:
+        problemas['outro'] = 'numero_chaves_invalido'
+
+    if 'titulo' in questao.keys() and len(questao[ 'titulo'].strip()) == 0:
+        problemas['titulo'] = 'vazio'
+
+    if 'nivel' in questao.keys() and questao['nivel'] not in ['facil', 'medio', 'dificil']:
+        problemas['nivel'] = 'valor_errado'
+
+    if 'opcoes' in questao.keys():
+        if len(questao['opcoes']) != 4:
+            problemas['opcoes'] = 'tamanho_invalido'
+        else:
+            for letra in ['A', 'B', 'C', 'D']:
+                if letra not in questao['opcoes'].keys():
+                    problemas['opcoes'] = 'chave_invalida_ou_nao_encontrada'
         
+        if 'opcoes' not in problemas.keys():
+            opcoes = {}
+            for alternativa, resposta in questao['opcoes'].items():
+                if len(str(resposta).strip()) == 0:
+                    opcoes[alternativa] = 'vazia'
+
+            if len(opcoes) > 0:
+                problemas['opcoes'] = opcoes
+    
+    if 'correta' in questao.keys():
+        if questao['correta'] not in ['A', 'B', 'C', 'D']:
+            problemas['correta'] = 'valor_errado'
+
+    return problemas
