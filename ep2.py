0ca4d2882348a0928bc1820157cc6961a5116577
marisavio <marianasavio16@gmail.com>
2022-07-28 10:40:41 -0300

    começo do jogo

--- ep2.py
+++ ep2.py
@@ -1,1 +1,20 @@
-print('hello')
+
+jogando = True 
+while jogando:
+
+
+    print('Olá! Você está na Fortuna DesSoft e terá a oportunidade de enriquecer!\n')
+    
+    nome = input('Qual seu nome? \n')
+
+    print(f'\nOk {nome}, você tem direito a pular 3 vezes e 2 ajudas!\nAs opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"!\n')
+    enter = input('Aperte ENTER para continuar...')
+    if enter == '':
+        print('\nO jogo já vai começar! Lá vem a primeira questão!\n\nVamos começar com questões do nível FACIL!')
+    enter = input('Aperte ENTER para continuar...')
+    if enter == '':
+        print('')
+'''
+    rodajogo = True 
+    while rodajogo:
+'''
